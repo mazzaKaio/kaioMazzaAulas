4d94a9ffb90372820dc26344353bbccdcbd3fd2f
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-04 21:35:13 -0300

    Update Exe01.py
    Adicionando mais um comentário - added line 7

--- Exe01.py
+++ Exe01.py
@@ -4,3 +4,4 @@
 # Gosto de: Programação, outros idiomas, The Mentalist, Animes, Java, Java e Java
 # Dr. House - CS 1.6 <3
 # Natural de SP - Suzano
+# Eu amo a aula de Zumba mas amo mesmo a de Programação de Aplicativos e o Prof Sergio
