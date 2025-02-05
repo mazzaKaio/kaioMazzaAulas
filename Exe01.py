a907241fbe4e5ded45f9f2d53781f859180fae9e
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-04 21:10:35 -0300

    Update Exe01.py
    Adicionando mais informações

--- Exe01.py
+++ Exe01.py
@@ -1,4 +1,6 @@
 # Kaio Gomes do Nascimento Mazza
 # Idade: 15 anos
 # Aniversário: 15/03
-# Gosto de: Programação, outros idiomas, The Mentalist
+# Gosto de: Programação, outros idiomas, The Mentalist, Animes, Java, Java e Java
+# Dr. House - CS 1.6 <3
+# Natural de SP - Suzano
