0f692bc92314c40c393d51c177261e8e001b970d
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-05 19:01:42 -0300

    Update Exe01.py
    Troca de um caractere na linha 5

--- Exe01.py
+++ Exe01.py
@@ -2,6 +2,6 @@
 # Idade: 15 anos
 # Aniversário: 15/03
 # Gosto de: Programação, outros idiomas, The Mentalist, Animes, Java, Java e Java
-# Dr. House - CS 1.6 <3
+# Dr. House & CS 1.6 <3
 # Natural de SP - Suzano
 # Eu amo a aula de Zumba mas amo mesmo a de Programação de Aplicativos e o Prof Sergio
