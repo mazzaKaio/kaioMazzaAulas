cb10800cb44fba5d5e9efbbd92d00dddfd48afc6
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-06 21:55:00 -0300

    exemplos feitos
    Exemplos feitos em sala de aula, base para o Python

---
+++ exemplo03.py
@@ -0,0 +1,5 @@
+# Faça um programa que receba um salario de R$1500 e tenha um aumento de 5% so salario e ao final exiba o salario com o aumento
+salario = 1500
+aumento = salario + (salario * 0.05)
+print("Salario base: ", salario)
+print("Salario com aumento de 5%: ", aumento)
