c009547ef4dc634206d91cd397837b6aa0c3747a
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-06 21:14:59 -0300

    Exercicio 1 de python

--- Exe01.py
+++ Exe01.py
@@ -1,7 +1,10 @@
-# Kaio Gomes do Nascimento Mazza
-# Idade: 15 anos
-# Aniversário: 15/03
-# Gosto de: Programação, outros idiomas, The Mentalist, Animes, Java, Java e Java
-# Dr. House & CS 1.6 <3
-# Natural de SP - Suzano
-# Eu amo a aula de Zumba mas amo mesmo a de Programação de Aplicativos e o Prof Sergio
+x = 34-23
+y = "Hello"
+z = 3.45
+
+if z == 3.45 or y == "Hello":
+    x = x + 1
+    y = y + " World"
+
+print(x)
+print(y)
