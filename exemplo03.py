cdc0caf2439da63279cbd64b2c08863b85e3c36c
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-06 22:04:01 -0300

    Update exemplo03.py
    Alteração no código, criação de variavel para indicar porcentagem do aumento

--- exemplo03.py
+++ exemplo03.py
@@ -1,5 +1,6 @@
 # Faça um programa que receba um salario de R$1500 e tenha um aumento de 5% so salario e ao final exiba o salario com o aumento
 salario = 1500
-aumento = salario + (salario * 0.05)
+aumento = 5
+salarioFinal = salario + (salario * aumento/100)
 print("Salario base: ", salario)
-print("Salario + 5%: ", aumento)
+print("Salario + 5%: ", salarioFinal)
