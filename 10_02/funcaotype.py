838566b69298bfff91c4786f63de008d86b496c5
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 19:24:08 -0300

    Create funcaotype.py

---
+++ 10_02/funcaotype.py
@@ -0,0 +1,11 @@
+faturamento = 1000
+print("Para varivael fundamento que recebeu 1000 o tipo é:", type(faturamento))
+
+faturamento = 1000.00
+print("Para varivael fundamento que recebeu 1000.00 o tipo é:", type(faturamento))
+
+faturamento = '1.000'
+print("Para varivael fundamento que recebeu '1.000' o tipo é:", type(faturamento))
+
+bonus = True
+print("Para varivael bonus que recebeu 'True' o tipo é:", type(bonus))
