cc490cef2911dffa2f0e8b79b398fcc5fac09826
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 19:44:25 -0300

    Create exemplo_string01.py

---
+++ 10_02/exemplo_string01.py
@@ -0,0 +1,11 @@
+primeiroNome = "ada"
+sobrenome = "wong"
+
+nomeCompleto = primeiroNome + " " + sobrenome
+print(nomeCompleto)
+
+print(nomeCompleto.title())
+
+nome = "Ada Wong"
+print(nome.upper())
+print(nome.lower())
