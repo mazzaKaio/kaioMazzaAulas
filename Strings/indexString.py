28ccc88bb95a65a0af7c4ceb0849d73ee887382f
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 19:59:32 -0300

    Create indexString.py

---
+++ Strings/indexString.py
@@ -0,0 +1,7 @@
+email = "SegioLuiz@gmail.com"
+
+print("\nPegando posição ZERO no email atribuido:")
+print(email[0])
+
+print("\nPegando da posição ZERO até a ONZE do email atribuido")
+print(email[0:11])
