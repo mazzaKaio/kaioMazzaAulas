fee78f53ab53ecc719ada4eb35991b0a7fc5239a
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 20:41:29 -0300

    Usando metodo LEN

--- Strings/indexString.py
+++ Strings/indexString.py
@@ -3,5 +3,8 @@ email = "mazzakaio@gmail.com"
 print("\nPegando posição ZERO no email atribuido:")
 print(email[0])
 
-print("\nPegando da posição ZERO até a ONZE do email atribuido")
+print("\nPegando da posição ZERO até o DEZ do email atribuido")
 print(email[0:10])
+
+print("\nUsando metodo LEN para saber o tamanho da variavel email:")
+print(len(email))
