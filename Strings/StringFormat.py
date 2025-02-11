c3a6cfa6373c6dfcc0efb65c454458a1934729f8
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 21:05:55 -0300

    Update StringFormat.py
    Usando format em uma só linha em 3 variaveis diferentes

--- Strings/StringFormat.py
+++ Strings/StringFormat.py
@@ -3,4 +3,4 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 
-print("O faturamento da loja foi de: {}, O custo foi de: {} e o Lucro foi de: {}".format(faturamento))
+print("O faturamento da loja foi de: {}, O custo foi de: {} e o Lucro foi de: {}".format(faturamento, custo, lucro))
