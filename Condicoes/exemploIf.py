7a3f2dbe74c8173f1ad69197e96a0962ed29b579
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-11 18:58:40 -0300

    Verificacao inteiro
    Verificando se número inserido é inteiro

--- Condicoes/exemploIf.py
+++ Condicoes/exemploIf.py
@@ -1,8 +1,11 @@
 num1 = int(input("Digite um número inteiro: "))
 
-if num1 > 10:
-    print(num1, "é maior do que 10")
-elif num1 == 10:
-    print(num1, "é igual a 10")
+if num1 == type(int):
+    if num1 > 10:
+        print(num1, "é maior do que 10")
+    elif num1 == 10:
+        print(num1, "é igual a 10")
+    else:
+        print(num1, "é menor do que 10")
 else:
-    print(num1, "é menor do que 10")
+    print("Número inserido não inteiro!")
