9dd7a2522c3e2b4096bb072b49efcccc6d731248
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-11 19:46:16 -0300

    Update atividadeIfElse.py
    Sem usar o ELIF

--- Condicoes/atividadeIfElse.py
+++ Condicoes/atividadeIfElse.py
@@ -3,7 +3,8 @@ num = int(input("Digite um número inteiro:"))
 
 if num > 20:
     print(num, "é maior que 20")
-elif num < 10:
-    print(num, "é menor que 10")
 else:
-    print(num, "está entre 10 e 20")
+    if num < 10:
+        print(num, "é menor que 10")
+    else:
+        print(num, "está entre 10 e 20")
