4d4ec89b35367abbc939709032ff7af9729dd5dd
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 21:02:06 -0300

    Update str_concatenacao.py

--- Strings/str_concatenacao.py
+++ Strings/str_concatenacao.py
@@ -3,6 +3,6 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 
-print("O faturamento da loja foi de: " + str(faturamento) + "\nO custo foi de: " + str(custo) + "\nO lucro foi de: " + str(lucro))
+print("O faturamento da loja foi de: " + str(faturamento))
 print("O custo foi de: " + str(custo))
 print("O lucro foi de: " + str(lucro))
