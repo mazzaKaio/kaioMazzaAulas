0770566df60a2140c1fbd72f9e737fdb638bf7ae
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 21:15:25 -0300

    Update StringFormat.py
    Usando as operações de strings

--- Strings/StringFormat.py
+++ Strings/StringFormat.py
@@ -4,3 +4,10 @@ custo = 500
 lucro = faturamento - custo
 
 print("O faturamento da loja foi de: {0}, O custo foi de: {1} e o Lucro foi de: {2}".format(faturamento, custo, lucro))
+
+# Uso do 'STR' e do concatenar com '+'
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+
+print("O faturamento da loja foi de: %s" %faturamento)
