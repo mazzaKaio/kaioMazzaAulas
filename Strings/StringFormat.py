821a33c507eb1f3729337042f61e16184c4de5f0
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 21:10:10 -0300

    Update StringFormat.py
    Usando indicies nos pares de chaves

--- Strings/StringFormat.py
+++ Strings/StringFormat.py
@@ -3,4 +3,4 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 
-print("O faturamento da loja foi de: {}, O custo foi de: {} e o Lucro foi de: {}".format(faturamento, custo, lucro))
+print("O faturamento da loja foi de: {0}, O custo foi de: {1} e o Lucro foi de: {2}".format(faturamento, custo, lucro))
