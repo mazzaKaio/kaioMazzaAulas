4b3a7d65cf3b693e868e5ce4b77fa2e05d4f4d4e
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-11 19:45:12 -0300

    Update atividade2IfElese.py
    Sem usar ELIF para fazer lógica

--- Condicoes/atividade2IfElese.py
+++ Condicoes/atividade2IfElese.py
@@ -5,7 +5,8 @@ notaAluno = float(input("Digite a nota do aluno:"))
 
 if notaAluno >= 7:
     print("O aluno foi APROVADO!")
-elif notaAluno < 5:
-    print("O aluno foi REPROVADO!")
 else:
-    print("O aluno está de RECUPERAÇÃO!")
+    if notaAluno >= 5:
+        print("O aluno está de RECUPERAÇÃO!")
+    else:
+        print("O aluno foi REPROVADO!")
