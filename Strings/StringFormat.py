8c8529854c0e9e47fecc7255351b60d5643108ba
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-10 21:00:29 -0300

    Usando Format

--- Strings/StringFormat.py
+++ Strings/StringFormat.py
@@ -3,4 +3,4 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 
-print("O faturamento da loja foi de: " + str(faturamento) + "\nO custo foi de: " + str(custo) + "\nO lucro foi de: " + str(lucro))
+print("O faturamento da loja foi de: {}".format(faturamento))
---
+++ Strings/str_concatenacao.py
@@ -0,0 +1,6 @@
+# Uso do 'STR' e do concatenar com '+'
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+
+print("O faturamento da loja foi de: " + str(faturamento) + "\nO custo foi de: " + str(custo) + "\nO lucro foi de: " + str(lucro))
