fed9aaa7d0e4754858c2abb6c0b2bfe7fd351515
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-17 20:58:32 -0300

    Exemplo continue

---
+++ LacosDeRepeticao/For/exemploContinue.py
@@ -0,0 +1,10 @@
+alunos = ["John Snow", "Jesse Pinkman", "Aria Stark", "Tyrion Lannister", "Voldemort"]
+chamado = "Aria Stark"
+
+for pessoa in alunos:
+    if pessoa.__eq__(chamado):
+        print("\n{} está presente!".format(chamado))
+        break
+    else:
+        print("{}...".format(pessoa))
+        continue
