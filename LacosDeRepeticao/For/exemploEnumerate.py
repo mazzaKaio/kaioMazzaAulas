f29f896f5ef56458370cf6a10f715e9cca77388a
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-17 20:07:27 -0300

    Exemplos laco for

---
+++ LacosDeRepeticao/For/exemploEnumerate.py
@@ -0,0 +1,4 @@
+funcionarios = ["Kaio", "Eberth", "Sergio", "Caio", "Daniel"]
+
+for i, funcionarios in enumerate(funcionarios):
+    print("{} é o funcionário {}".format(funcionarios, i + 1))
---
+++ LacosDeRepeticao/For/exemploEstoque.py
@@ -0,0 +1,6 @@
+produtos = ["Coca cola", "pepsi", "guanara", "fanta", "sprite", "h2o", "skol", "brahma", "tubaina", "dolly guarana"]
+estoque = [350, 250 , 876, 15, 150, 346, 74, 25, 1, 34]
+nivelMinimo = 50
+
+for qntd in estoque:
+    print(qntd)
