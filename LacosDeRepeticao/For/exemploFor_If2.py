a7180d34eed76a669e0ca7380863d906923b4b46
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-17 19:50:00 -0300

    Resolucao exe. 02

---
+++ LacosDeRepeticao/For/exemploFor_If2.py
@@ -0,0 +1,11 @@
+vendas = [100, 50, 80, 190, 200, 210, 45, 37, 99, 105, 130, 111, 44, 24, 67, 93, 157, 25]
+meta = 100
+bateramMeta = 0
+
+for venda in vendas:
+    if venda >= meta:
+        print(venda)
+        bateramMeta += 1
+
+numFuncionarios = len(vendas)
+print("O percentual de funcionários que bateram a meta foi de {:.1%}".format(bateramMeta/numFuncionarios))
---
+++ LacosDeRepeticao/For/exemplorFor_If.py
@@ -0,0 +1,6 @@
+vendas = [100, 50, 80, 190, 200, 210, 45, 37, 99, 105, 130, 111, 44, 24, 67, 93, 157, 25]
+meta = 100
+
+for venda in vendas:
+    if venda >= meta:
+        print(venda)
