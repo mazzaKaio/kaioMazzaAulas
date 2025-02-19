7fe36b16e3251e550ff54f038dd4518ba7a839dd
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-19 19:15:41 -0300

    unpacking

---
+++ Tupas/exemplo01.py
@@ -0,0 +1,8 @@
+funcionario = ("Kaio", "17/02/2027", "15/03/2009", "6750", "Senior Full-Stack")
+nome, dataContratacao, dataNascimento, salario, cargo = funcionario
+
+print(nome)
+print(dataContratacao)
+print(dataNascimento)
+print(salario)
+print(cargo)
